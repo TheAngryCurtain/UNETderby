81e005040ec86a8188c9f493457d5aaa7ce2f7e3
Mike <mikes@hb-studios.com>
2017-02-03 17:09:24 -0400

    more changes

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -4,7 +4,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 {
 	[RequireComponent(typeof(Rigidbody))]
 	[RequireComponent(typeof(CapsuleCollider))]
-	[RequireComponent(typeof(Animator))]
 	public class ThirdPersonCharacter : MonoBehaviour
 	{
 		[SerializeField] float m_MovingTurnSpeed = 360;
@@ -17,7 +16,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		[SerializeField] float m_GroundCheckDistance = 0.1f;
 
 		Rigidbody m_Rigidbody;
-		Animator m_Animator;
+		//Animator m_Animator;
 		bool m_IsGrounded;
 		float m_OrigGroundCheckDistance;
 		const float k_Half = 0.5f;
@@ -32,7 +31,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		void Start()
 		{
-			m_Animator = GetComponent<Animator>();
+			//m_Animator = GetComponent<Animator>();
 			m_Rigidbody = GetComponent<Rigidbody>();
 			m_Capsule = GetComponent<CapsuleCollider>();
 			m_CapsuleHeight = m_Capsule.height;
@@ -72,7 +71,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			PreventStandingInLowHeadroom();
 
 			// send input and other state parameters to the animator
-			UpdateAnimator(move);
+			//UpdateAnimator(move);
 		}
 
 
@@ -115,42 +114,42 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		}
 
 
-		void UpdateAnimator(Vector3 move)
-		{
-			// update the animator parameters
-			m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
-			m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
-			m_Animator.SetBool("Crouch", m_Crouching);
-			m_Animator.SetBool("OnGround", m_IsGrounded);
-			if (!m_IsGrounded)
-			{
-				m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
-			}
+        //void UpdateAnimator(Vector3 move)
+        //{
+        //    // update the animator parameters
+        //    m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
+        //    m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
+        //    m_Animator.SetBool("Crouch", m_Crouching);
+        //    m_Animator.SetBool("OnGround", m_IsGrounded);
+        //    if (!m_IsGrounded)
+        //    {
+        //        m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
+        //    }
 
-			// calculate which leg is behind, so as to leave that leg trailing in the jump animation
-			// (This code is reliant on the specific run cycle offset in our animations,
-			// and assumes one leg passes the other at the normalized clip times of 0.0 and 0.5)
-			float runCycle =
-				Mathf.Repeat(
-					m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
-			float jumpLeg = (runCycle < k_Half ? 1 : -1) * m_ForwardAmount;
-			if (m_IsGrounded)
-			{
-				m_Animator.SetFloat("JumpLeg", jumpLeg);
-			}
+        //    // calculate which leg is behind, so as to leave that leg trailing in the jump animation
+        //    // (This code is reliant on the specific run cycle offset in our animations,
+        //    // and assumes one leg passes the other at the normalized clip times of 0.0 and 0.5)
+        //    float runCycle =
+        //        Mathf.Repeat(
+        //            m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
+        //    float jumpLeg = (runCycle < k_Half ? 1 : -1) * m_ForwardAmount;
+        //    if (m_IsGrounded)
+        //    {
+        //        m_Animator.SetFloat("JumpLeg", jumpLeg);
+        //    }
 
-			// the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
-			// which affects the movement speed because of the root motion.
-			if (m_IsGrounded && move.magnitude > 0)
-			{
-				m_Animator.speed = m_AnimSpeedMultiplier;
-			}
-			else
-			{
-				// don't use that while airborne
-				m_Animator.speed = 1;
-			}
-		}
+        //    // the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
+        //    // which affects the movement speed because of the root motion.
+        //    if (m_IsGrounded && move.magnitude > 0)
+        //    {
+        //        m_Animator.speed = m_AnimSpeedMultiplier;
+        //    }
+        //    else
+        //    {
+        //        // don't use that while airborne
+        //        m_Animator.speed = 1;
+        //    }
+        //}
 
 
 		void HandleAirborneMovement()
